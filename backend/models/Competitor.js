06a924477f18c97e77087ab4b4939e9f46f0156a
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2026-03-12 08:00:56 +0530

    update Competitor.js

--- backend/models/Competitor.js
+++ backend/models/Competitor.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose";
 
 const competitorSchema = new mongoose.Schema({
   name: String,
-  round_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Round"
-  }
+  round_id: String,
+  photo: String // NEW FIELD
 });
 
 export default mongoose.model("Competitor", competitorSchema);
